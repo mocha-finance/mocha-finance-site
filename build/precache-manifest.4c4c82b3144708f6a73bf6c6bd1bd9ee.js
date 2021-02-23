902475e9d132e78e122b97c09439c50ed9f5a734
🔏 - Frantz Bazile <Frantz@kargo.com>
2021-02-23 06:48:10 -0500

    🥋 - Adding build folder
    Signed-off-by: 🔏 - Frantz Bazile <Frantz@kargo.com>

---
+++ build/precache-manifest.4c4c82b3144708f6a73bf6c6bd1bd9ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8678941c41d766c34166a3bbf4421f44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8d39290a41c2c837e1b",
+    "url": "/static/css/main.fd7805e7.chunk.css"
+  },
+  {
+    "revision": "a42541a06cc3e04549b1",
+    "url": "/static/js/2.7c8eff5f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7c8eff5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8d39290a41c2c837e1b",
+    "url": "/static/js/main.0f03d71b.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
