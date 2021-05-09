1483200b068dd0094686293771946fa1a06dd289
🔏 - Frantz Bazile <Frantz@kargo.com>
2021-05-09 14:42:18 -0400

    💻 🚀  - Adding Mocha contract
    Signed-off-by: 🔏 - Frantz Bazile <Frantz@kargo.com>

---
+++ src/contracts/Mocha.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.5.0;
+
+contract EXTX {
+    string  public name = "Mocha Token";
+    string  public symbol = "Moch";
+    address public owner;
+    uint256 public totalSupply = 10000000e18; // 10 million tokens
+    uint8   public decimals = 18;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor() public {
+        balanceOf[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+    
+    function mint(address account, uint256 amount) public {
+        require(account != address(0), "ERC20: mint to the zero address");
+        require(msg.sender == owner, "caller must be owner");
+
+        totalSupply += amount;
+        balanceOf[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+    
+    function burn(address account, uint256 amount) public {
+        require(account != address(0), "ERC20: burn from the zero address");
+        require(msg.sender == owner, "caller must be owner");
+
+        uint256 accountBalance = balanceOf[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        balanceOf[account] = accountBalance - amount;
+        totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+    }
+}
